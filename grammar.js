cdc2204d40cc667b4dbb2203223624343a7edbd2
Omar Valdez <omarantoniovaldezf2@gmail.com>
2024-07-02 07:12:00 -0700

    style: Format grammar with prettier

--- grammar.js
+++ grammar.js
@@ -28,43 +28,29 @@ module.exports = grammar({
   word: $ => $._anything,
 
   rules: {
-    document: $ => seq(
-      alias(repeat($._line), $.preamble),
-      repeat($.section),
-    ),
+    document: $ => seq(alias(repeat($._line), $.preamble), repeat($.section)),
 
     comment: _ => /[#;].*/,
+    _line: $ => choice($.pair, $.comment, NEWLINE),
 
-    _line: $ => choice(
-      $.pair,
-      $.comment,
-      NEWLINE,
-    ),
-
-    section: $ => seq(
-      $._section_header,
-      repeat($._line)
-    ),
-
-    _section_header: $ => seq(
-      '[',
-      alias($._glob_expression, $.section_name),
-      ']',
-      $._newline
-    ),
+    section: $ => seq($._section_header, repeat($._line)),
+    _section_header: $ => seq('[', alias($._glob_expression, $.section_name), ']', $._newline),
 
     // https://spec.editorconfig.org/#glob-expressions
-    _glob_expression: $ => repeat1(choice(
-      alias('*', $.wildcard_chars),
-      alias('**', $.wildcard_chars_allow_slash),
-      alias('?', $.wildcard_char_single),
-      alias('/', $.path_separator),
-      alias(/[^?*/\n{}\[\]]/, $.character),
-      $.escaped_character,
-      $.integer_range,
-      $.brace_expansion,
-      $.character_choice,
-    )),
+    _glob_expression: $ =>
+      repeat1(
+        choice(
+          alias('*', $.wildcard_chars),
+          alias('**', $.wildcard_chars_allow_slash),
+          alias('?', $.wildcard_char_single),
+          alias('/', $.path_separator),
+          alias(/[^?*/\n{}\[\]]/, $.character),
+          $.escaped_character,
+          $.integer_range,
+          $.brace_expansion,
+          $.character_choice,
+        ),
+      ),
 
     escaped_character: _ => token(seq('\\', token.immediate(/\W/))),
 
@@ -72,76 +58,75 @@ module.exports = grammar({
     brace_expansion: $ => seq('{', repeat(choice(',', $.expansion_string)), '}'),
     expansion_string: $ => prec.left(repeat1(prec.right($._glob_expression))),
 
-    integer_range: $ => seq(
-      '{',
-      field('start', alias($._integer_range_start, $.number)),
-      token.immediate('..'),
-      field('end', alias(token.immediate(/-?\d+/), $.number)),
-      '}',
-    ),
+    integer_range: $ =>
+      seq(
+        '{',
+        field('start', alias($._integer_range_start, $.number)),
+        token.immediate('..'),
+        field('end', alias(token.immediate(/-?\d+/), $.number)),
+        '}',
+      ),
 
     character_choice: $ => seq('[', $._character_choice_content, ']'),
 
-    _character_choice_content: $ => seq(
-      optional(alias(token.immediate('!'), $.negation)),
-      repeat1(choice(
-        $.character_range,
-        $.escaped_character,
-        alias(/[^\]\r\n/]/, $.character),
-      )),
-    ),
+    _character_choice_content: $ =>
+      seq(
+        optional(alias(token.immediate('!'), $.negation)),
+        repeat1(choice($.character_range, $.escaped_character, alias(/[^\]\r\n/]/, $.character))),
+      ),
 
-    character_range: $ => seq(
-      field('start', alias(/[^\]\r\n/]/, $.character)),
-      token.immediate('-'),
-      field('end', alias(token.immediate(/\w/), $.character)),
-    ),
+    character_range: $ =>
+      seq(
+        field('start', alias(/[^\]\r\n/]/, $.character)),
+        token.immediate('-'),
+        field('end', alias(token.immediate(/\w/), $.character)),
+      ),
 
-    pair: $ => seq(
-      field('key', alias(/[^;#=\s\[][^=\n]*/, $.identifier)),
-      '=',
-      /[ \t]*/, // Eat all the leading white-space
-      field('value', $._value),
-      $._newline,
-    ),
+    pair: $ =>
+      seq(
+        field('key', alias(/[^;#=\s\[][^=\n]*/, $.identifier)),
+        '=',
+        /[ \t]*/, // Eat all the leading white-space
+        field('value', $._value),
+        $._newline,
+      ),
 
-    _value: $ => choice(
-      // The spec allows the use of arbitrary values even if they are not supported
-      // so this capture is used as a fallback if no supported values match
-      alias($._anything, $.other),
-      alias(toCaseInsensitive('unset'), $.unset),
-      alias(/\d+/, $.number),
-      alias(/[a-z]{2}-[A-Z]{2}/, $.spelling_language),
-      ...makeValues($.boolean, 'true', 'false', 'off'),
-      ...makeValues($.indent_style, 'space', 'tab'),
-      ...makeValues($.end_of_line, 'lf', 'cr', 'crlf'),
-      ...makeValues($.charset, 'latin1', 'utf-8', 'utf-16be', 'utf-16le', 'utf-8-bom'),
-    ),
+    _value: $ =>
+      choice(
+        // The spec allows the use of arbitrary values even if they are not supported
+        // so this capture is used as a fallback if no supported values match
+        alias($._anything, $.other),
+        alias(toCaseInsensitive('unset'), $.unset),
+        alias(/\d+/, $.number),
+        alias(/[a-z]{2}-[A-Z]{2}/, $.spelling_language),
+        ...makeValues($.boolean, 'true', 'false', 'off'),
+        ...makeValues($.indent_style, 'space', 'tab'),
+        ...makeValues($.end_of_line, 'lf', 'cr', 'crlf'),
+        ...makeValues($.charset, 'latin1', 'utf-8', 'utf-16be', 'utf-16le', 'utf-8-bom'),
+      ),
 
     _anything: _ => /.*\S/,
     _newline: $ => choice(NEWLINE, $._end_of_file),
-  }
+  },
 });
-
 
 /**
  * @param {SymbolRule<string>} namedNode The name of the node to alias these values
  * @param {...string} values Supported values defined in the spec
  */
 function makeValues(namedNode, ...values) {
-  return values.map(v => alias(toCaseInsensitive(v), namedNode))
+  return values.map(v => alias(toCaseInsensitive(v), namedNode));
 }
 
-
 /**
- * @param {string} str String to make case insensitive
+ * @param {string} string String to make case insensitive
  * @returns {RegExp} Case insensitive regex
  */
-function toCaseInsensitive(str) {
+function toCaseInsensitive(string) {
   const alphaChar = /[a-zA-Z]/;
-  return new RegExp(str.split('').map(
-    char => alphaChar.test(char)
-      ? '[' + char.toLowerCase() + char.toUpperCase() + ']'
-      : char
-  ).join(''));
+  const characters = string.split('');
+  const upperLowerChars = characters.map(char =>
+    alphaChar.test(char) ? '[' + char.toLowerCase() + char.toUpperCase() + ']' : char,
+  );
+  return new RegExp(upperLowerChars.join(''));
 }

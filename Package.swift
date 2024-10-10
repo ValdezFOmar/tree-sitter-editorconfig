efbe0b2f2eaa248e8c657a80a8c2305056bda477
Omar Valdez <omarantoniovaldezf2@gmail.com>
2024-10-10 04:16:36 -0700

    build(bindings): update bindings and manifest files

--- Package.swift
+++ Package.swift
@@ -3,46 +3,35 @@ import PackageDescription
 
 let package = Package(
     name: "TreeSitterEditorconfig",
-    platforms: [.macOS(.v10_13), .iOS(.v11)],
     products: [
         .library(name: "TreeSitterEditorconfig", targets: ["TreeSitterEditorconfig"]),
     ],
-    dependencies: [],
+    dependencies: [
+        .package(url: "https://github.com/ChimeHQ/SwiftTreeSitter", from: "0.8.0"),
+    ],
     targets: [
-        .target(name: "TreeSitterEditorconfig",
-                path: ".",
-                exclude: [
-                    "Cargo.toml",
-                    "Makefile",
-                    "binding.gyp",
-                    "bindings/c",
-                    "bindings/go",
-                    "bindings/node",
-                    "bindings/python",
-                    "bindings/rust",
-                    "prebuilds",
-                    "grammar.js",
-                    "package.json",
-                    "package-lock.json",
-                    "pyproject.toml",
-                    "setup.py",
-                    "test",
-                    "examples",
-                    ".editorconfig",
-                    ".github",
-                    ".gitignore",
-                    ".gitattributes",
-                    ".gitmodules",
-                ],
-                sources: [
-                    "src/parser.c",
-                    "src/scanner.c",
-                ],
-                resources: [
-                    .copy("queries")
-                ],
-                publicHeadersPath: "bindings/swift",
-                cSettings: [.headerSearchPath("src")])
+        .target(
+            name: "TreeSitterEditorconfig",
+            dependencies: [],
+            path: ".",
+            sources: [
+                "src/parser.c",
+                "src/scanner.c",
+            ],
+            resources: [
+                .copy("queries")
+            ],
+            publicHeadersPath: "bindings/swift",
+            cSettings: [.headerSearchPath("src")]
+        ),
+        .testTarget(
+            name: "TreeSitterEditorconfigTests",
+            dependencies: [
+                "SwiftTreeSitter",
+                "TreeSitterEditorconfig",
+            ],
+            path: "bindings/swift/TreeSitterEditorconfigTests"
+        )
     ],
     cLanguageStandard: .c11
 )

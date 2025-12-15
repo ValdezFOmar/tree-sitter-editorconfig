84076ffc779e1473ca91752e47cd3844d4d9f4fd
Omar Valdez <omarantoniovaldezf2@gmail.com>
2025-12-15 06:50:08 -0800

    chore: release v1.2.0

--- src/parser.c
+++ src/parser.c
@@ -2600,8 +2600,8 @@ TS_PUBLIC const TSLanguage *tree_sitter_editorconfig(void) {
     .max_reserved_word_set_size = 0,
     .metadata = {
       .major_version = 1,
-      .minor_version = 1,
-      .patch_version = 2,
+      .minor_version = 2,
+      .patch_version = 0,
     },
   };
   return &language;
